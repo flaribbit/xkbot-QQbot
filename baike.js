21974ef8e969b90219b7dc1a2f1ec4e3984242a0
flaribbit <1149761294@qq.com>
2020-06-15 08:18:13 +0800

    baike中callback引入

--- baike.js
+++ baike.js
@@ -1,10 +1,9 @@
 const https = require("https");
 const cheerio = require("cheerio");
-const STRING_AT="[CQ:at,qq=]";
 
 function getHTML(url, callback) {
     https.get(url, function (res) {
-        if(res.statusCode==200){
+        if (res.statusCode == 200) {
             var data = "";
             res.on("data", function (chunk) {
                 data += chunk;
@@ -13,48 +12,39 @@ function getHTML(url, callback) {
                 callback(data);
                 return true;
             });
-        }else if(res.statusCode==302){
-            if(res.headers.location.search("item")>-1){
-                getHTML("https://baike.baidu.com"+res.headers.location,callback);
-            }else{
-                console.log("没有查询到该词条");
+        } else if (res.statusCode == 302) {
+            if (res.headers.location.search("item") > -1) {
+                getHTML("https://baike.baidu.com" + res.headers.location, callback);
+            } else {
+                callback(null,"没有查询到该词条");
                 return false;
             }
         }
     }).on("error", function () {
-        console.log("http get error");
+        callback(null,"http get error");
         return false;
-    }).setTimeout(10000,function(){
-        console.log("请求超时");
+    }).setTimeout(10000, function () {
+        callback(null,"请求超时");
     });
 }
 
-function search(keyword) {
-    getHTML("https://baike.baidu.com/item/"+keyword,function (data){
-        var $=cheerio.load(data);
-        var content=$(".lemma-summary").text().trim();
-        if(content.length>200){
-            content=content.substr(0,200)+"...";
-        }
-        console.log(content);
-    });
-}
-
-function SendGroupMessage(ws,group_id,message){
-    ws.send(JSON.stringify({
-        "action": "send_group_msg",
-        "params": {
-            "group_id": group_id,
-            "message": message
-        }
-    }));
-}
-
-exports.check = function (message) {
-    if(message.indexOf(STRING_AT)==0){
-        var res=message.replace(STRING_AT,"").trim().match(/(.*?)是(谁|什么)/);
-        if(res){
-            search(res[1]);
+exports.check = function (message, send) {
+    var text = message.message;
+    var at = `[CQ:at,qq=${self_id}]`;
+    if (text.indexOf(at) == 0) {
+        text = message.replace(at, "").trim().match(/(.*?)是(谁|什么)/);
+        if (res) {
+            getHTML("https://baike.baidu.com/item/" + res[1], function (data, error) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    var $ = cheerio.load(data);
+                    var content = $(".lemma-summary").text().trim();
+                    if (content.length > 200) {
+                        content = content.substr(0, 200) + "...";
+                    }
+                }
+            });
         }
     }
 }
--- main.js
+++ main.js
@@ -7,8 +7,19 @@ ws.onopen=function(){
     console.log("ws已连接");
 }
 ws.onmessage=function(ev){
-    var message=JSON.parse(ev.data).message;
-    if(message){
-        baike.check(message);
+    var message=JSON.parse(ev.data);
+    if(message.message){
+        // console.log(message);
+        baike.check(message,SendGroupMessage);
     }
+}
+
+function SendGroupMessage(ws,group_id,message){
+    ws.send(JSON.stringify({
+        "action": "send_group_msg",
+        "params": {
+            "group_id": group_id,
+            "message": message
+        }
+    }));
 }
