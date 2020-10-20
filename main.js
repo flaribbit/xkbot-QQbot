13690be99c6b008dba1c2a67bc760f4871bb6164
flaribbit <1149761294@qq.com>
2020-10-20 16:54:59 +0800

    改用httpapi

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
-const WebSocket=require("ws")
-var baike=require("./baike")
-var dice=require("./dice")
-var zhihu=require("./zhihu")
 
-var ws=new WebSocket("ws://127.0.0.1:6700")
-ws.onopen=function(){
-    console.log("ws已连接");
-}
-ws.onmessage=function(ev){
-    var message=JSON.parse(ev.data);
-    if(message.message){
-        // console.log(message);
-        dice.check(message,SendGroupMessage);
-        baike.check(message,SendGroupMessage);
-        zhihu.check(message,SendGroupMessage);
-    }
-}
+var http = require('http');
+var baike = require("./baike")
+var dice = require("./dice")
+var zhihu = require("./zhihu")
 
-function SendGroupMessage(group_id,message){
-    ws.send(JSON.stringify({
-        "action": "send_group_msg",
-        "params": {
-            "group_id": group_id,
-            "message": message
-        }
+http.createServer(function (req, res) {
+    var data = "";
+    req
+        .on("data", d => data += d)
+        .on("end", () => {
+            var message = JSON.parse(data);
+            if (message.message_type && message.message_type == "group") {
+                console.log(data);
+                dice.check(message, SendGroupMessage);
+                baike.check(message, SendGroupMessage);
+                zhihu.check(message, SendGroupMessage);
+            }
+        });
+    res.end();
+}).listen(5701);
+
+function SendGroupMessage(group_id, message) {
+    var req = http.request("http://localhost:5700", {
+        path: "/send_group_msg",
+        method: "POST",
+    }, res => {
+        res.on("data", data => {
+            console.log(data.toString());
+        });
+    });
+    req.write(JSON.stringify({
+        "group_id": group_id,
+        "message": message
     }));
+    req.end();
+    console.log("发送");
 }
