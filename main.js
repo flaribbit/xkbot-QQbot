31ea853d3d59bdb3877cfa006f84f10a152a6277
flaribbit <1149761294@qq.com>
2020-10-22 11:12:13 +0800

    代码格式化

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var http = require('http');
-var baike = require("./baike")
-var dice = require("./dice")
-var zhihu = require("./zhihu")
+var baike = require("./baike");
+var dice = require("./dice");
+var zhihu = require("./zhihu");
+var trivia = require("./trivia");
 
 http.createServer(function (req, res) {
     var data = "";
@@ -12,6 +13,7 @@ http.createServer(function (req, res) {
             if (message.message_type) {
                 console.log(data);
                 dice.check(message);
+                trivia.check(message);
             }
         });
     res.end();
