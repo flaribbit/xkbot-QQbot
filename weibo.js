67bd216e473d24c9880472e88aba13aa404b1e6e
flaribbit <1149761294@qq.com>
2020-10-31 11:42:30 +0800

    update: weibo获取最新一条微博

--- weibo.js
+++ weibo.js
@@ -1,15 +1,29 @@
 const bot = require("./bot");
 const { default: Axios } = require("axios");
 
-//api: https://m.weibo.cn/api/container/getIndex?containerid=107603${id}
-Axios.get("https://m.weibo.cn/api/container/getIndex?containerid=1076031470809487").then(res => {
-    res.data.data.cards.forEach(card => {
-        if (card.card_type != 9) return;
-        var text = card.mblog.text.replace(/<span class="url-icon"><img alt=([^ ]+).*?\/span>/g, "$1");
-        // text = text.replace(/<a href="\/status.*?\/a>/, "");
-        text = text.replace(/<a .*?>(.*?)<\/a>/g, "$1");
-        text = text.replace(/<span .*?>(.*?)<\/span>/g, "$1");
-        console.log(text);
-        console.log(card.scheme.split("?")[0]);
+exports.check = function (message) {
+    var text = message.message;
+    var send = message.message_type == "group" ? bot.SendGroupMessage : bot.SendPrivateMessage;
+    var sender = message.sender.card || message.sender.nickname;
+    var target = message.group_id || message.user_id;
+    var res = text.match(/^.weibo ?(\d+)$/);
+    if (res) {
+        sendLatestById(res[1], send, target);
+        return true;
+    }
+}
+
+function sendLatestById(id, send, target) {
+    Axios.get("https://m.weibo.cn/api/container/getIndex?containerid=107603" + id).then(res => {
+        var cards = res.data.data.cards;
+        for (var i = 0; i < cards.length; i++) {
+            if (cards[i].card_type == 9) {
+                var text = cards[i].mblog.text.replace(/<span class="url-icon"><img alt=([^ ]+).*?\/span>/g, "$1");
+                text = text.replace(/<a .*?>(.*?)<\/a>/g, "$1");
+                text = text.replace(/<span .*?>(.*?)<\/span>/g, "$1");
+                send(target, text + "\n" + cards[i].scheme.split("?")[0]);
+                return;
+            }
+        }
     });
-});
+}
