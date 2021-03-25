305cbf8c99936ba903f4ed119c9c92c4aaecf122
flaribbit <1149761294@qq.com>
2021-03-25 20:09:08 +0800

    bvinfo改为await promise

--- bvinfo.js
+++ bvinfo.js
@@ -1,9 +1,9 @@
 const bot = require("./bot");
-const { default: Axios } = require("axios");
+const { default: axios } = require("axios");
 const dayjs = require("dayjs")
 
 const API = "http://api.bilibili.com/x/web-interface/view";
-exports.check = function (message) {
+exports.check = async function (message) {
     if (message.message_type == "group") {
         var send = bot.SendGroupMessage, target = message.group_id;
     } else {
@@ -12,38 +12,48 @@ exports.check = function (message) {
     var text = message.message;
     var res = text.match(/BV[0-9a-zA-Z]{10}/);
     if (res) {
-        Axios.get(API + "?bvid=" + res[0]).then(({ "data": {
-            "data": {
-                "bvid": bvid,
-                "aid": avid,
-                "pic": pic,
-                "title": title,
-                "pubdate": pubdate,
-                "desc": desc,
-                "owner": {
-                    "name": upname,
-                },
-                "stat": {
-                    "view": view,
-                    "danmaku": danmaku,
-                    "reply": reply,
-                    "favorite": favorite,
-                    "coin": coin,
-                    "like": like,
-                }
+        send(target, await getInfo(res[0]));
+        return true;
+    }
+    res = text.match(/https:\/\/b23.tv\/[0-9a-zA-Z]+/);
+    if (res) {
+        res = await axios.get(res[0], { maxRedirects: 0 }).then(_ => {
+        }, err => err.response.headers.location.match(/BV[0-9a-zA-Z]{10}/));
+        send(target, await getInfo(res[0]));
+        return true;
+    }
+    return false;
+}
+
+function getInfo(bv) {
+    return axios.get(API + "?bvid=" + bv).then(({ "data": {
+        "data": {
+            "bvid": bvid,
+            "aid": avid,
+            "pic": pic,
+            "title": title,
+            "pubdate": pubdate,
+            "desc": desc,
+            "owner": {
+                "name": upname,
+            },
+            "stat": {
+                "view": view,
+                "danmaku": danmaku,
+                "reply": reply,
+                "favorite": favorite,
+                "coin": coin,
+                "like": like,
             }
-        } }) => {
-            send(target, `${bot.Image(pic)}
+        }
+    } }) => `${bot.Image(pic)}
 @${upname} b23.tv/av${avid}
 ${dayjs(pubdate * 1000).format("YYYY/MM/DD HH:mm:ss")}
 ${title}
-${Str(view)}播放 ${Str(coin)}硬币`);
-            return true;
-        });
-    }
+${str(view)}播放 ${str(coin)}硬币`);
 }
 
-function Str(n) {
+function str(n) {
     if (n > 1e5) {
         return (n / 10000).toFixed(1) + "w";
     } else {
