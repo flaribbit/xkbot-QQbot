f26d48f4eb8cab9f90f3d284c61551d75d7a79dc
flaribbit <1149761294@qq.com>
2021-11-30 15:25:19 +0800

    bvinfo插件迁移

--- plugins/bvinfo.js
+++ plugins/bvinfo.js
@@ -1,25 +1,21 @@
-const bot = require("./bot");
+const { image } = require("../bot");
 const { default: axios } = require("axios");
-const dayjs = require("dayjs")
+const dayjs = require("dayjs");
 
 const API = "http://api.bilibili.com/x/web-interface/view";
-exports.check = async function (message) {
-    if (message.message_type == "group") {
-        var send = bot.SendGroupMessage, target = message.group_id;
-    } else {
-        var send = bot.SendPrivateMessage, target = message.user_id;
-    }
+exports.name = "bvinfo";
+exports.check = async function (message, info, reply) {
     var text = message.message;
     var res = text.match(/BV[0-9a-zA-Z]{10}/);
     if (res) {
-        send(target, await getInfo(res[0]));
+        reply(await getInfo(res[0]));
         return true;
     }
     res = text.match(/https:\/\/b23.tv\/[0-9a-zA-Z]+/);
     if (res) {
         res = await axios.get(res[0], { maxRedirects: 0 }).then(_ => {
         }, err => err.response.headers.location.match(/BV[0-9a-zA-Z]{10}/));
-        send(target, await getInfo(res[0]));
+        reply(await getInfo(res[0]));
         return true;
     }
     return false;
@@ -46,11 +42,11 @@ function getInfo(bv) {
                 "like": like,
             }
         }
-    } }) => `${bot.Image(pic)}
+    } }) => `${image(pic)}
 ${upname} b23.tv/av${avid}
 ${title}
 ${dayjs(pubdate * 1000).format("YYYY/MM/DD HH:mm:ss")}
-${str(view)}播放 ${str(coin)}硬币`);
+${str(view)}播放 ${str(link)}点赞 ${str(coin)}硬币`);
 }
 
 function str(n) {
