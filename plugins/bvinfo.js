256ffe4bf074bc1830ecbbca977345e02c24ddb2
flaribbit <1149761294@qq.com>
2021-11-30 19:11:27 +0800

    修复bvinfo

--- plugins/bvinfo.js
+++ plugins/bvinfo.js
@@ -4,49 +4,35 @@ const dayjs = require("dayjs");
 
 const API = "http://api.bilibili.com/x/web-interface/view";
 exports.name = "bvinfo";
-exports.check = async function (message, info, reply) {
-    var text = message.message;
-    var res = text.match(/BV[0-9a-zA-Z]{10}/);
-    if (res) {
+exports.handle = async function (message, info, reply) {
+    const text = message.message;
+    var res;
+    if (res = text.match(/BV[0-9a-zA-Z]{10}/)) {
         reply(await getInfo(res[0]));
         return true;
     }
-    res = text.match(/https:\/\/b23.tv\/[0-9a-zA-Z]+/);
-    if (res) {
-        res = await axios.get(res[0], { maxRedirects: 0 }).then(_ => {
-        }, err => err.response.headers.location.match(/BV[0-9a-zA-Z]{10}/));
+    if (res = text.match(/https:\/\/b23.tv\/[0-9a-zA-Z]+/)) {
+        res = await axios.get(res[0], { maxRedirects: 0 }).catch(err => err.response.headers.location.match(/BV[0-9a-zA-Z]{10}/));
         reply(await getInfo(res[0]));
         return true;
     }
     return false;
 }
 
-function getInfo(bv) {
-    return axios.get(API + "?bvid=" + bv).then(({ "data": {
+async function getInfo(bv) {
+    const { "data": {
         "data": {
-            "bvid": bvid,
-            "aid": avid,
-            "pic": pic,
-            "title": title,
-            "pubdate": pubdate,
-            "desc": desc,
-            "owner": {
-                "name": upname,
-            },
-            "stat": {
-                "view": view,
-                "danmaku": danmaku,
-                "reply": reply,
-                "favorite": favorite,
-                "coin": coin,
-                "like": like,
-            }
+            "bvid": bvid, "aid": avid, "pic": pic, "title": title, "pubdate": pubdate, "desc": desc,
+            "owner": { "name": upname },
+            "stat": { "view": view, "danmaku": danmaku, "reply": reply, "favorite": favorite, "coin": coin, "like": like },
         }
-    } }) => `${image(pic)}
-${upname} b23.tv/av${avid}
+    } } = await axios.get(API + "?bvid=" + bv);
+    return `${image(pic)}
+${upname} b23.tv/av${avid} ${bvid}
 ${title}
 ${dayjs(pubdate * 1000).format("YYYY/MM/DD HH:mm:ss")}
-${str(view)}播放 ${str(link)}点赞 ${str(coin)}硬币`);
+${str(view)}播放 ${str(link)}点赞
+${str(coin)}硬币 ${str(favorite)}收藏`;
 }
 
 function str(n) {
