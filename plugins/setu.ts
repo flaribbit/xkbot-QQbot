07628b7b0af02e23a4f3c440f72e046e41bb76ab
flaribbit <1149761294@qq.com>
2022-01-20 15:07:58 +0800

    更新色图插件
    - 学到了，`type` 可以放到花括号里面
    - 捕获axios报错

--- plugins/setu.ts
+++ plugins/setu.ts
@@ -1,27 +1,28 @@
-import { image } from "../bot";
-import type { Handle } from "../bot";
-import axios from "axios";
-const API = "https://setu.flaribbit.workers.dev/";
+import { type Handle, image } from "../bot"
+import axios from "axios"
+const API = "https://setu.flaribbit.workers.dev/"
 
-export let name = "色图";
-export let handle: Handle = function (message, reply, info) {
-    const text = message.message;
+export const name = "色图"
+export const handle: Handle = function (message, reply, info) {
+    const text = message.message
     if (message.message_type == "group") {
-        var res = text.match(/^\[CQ:at,qq=(\d+)\] ?来张色图$/);
+        const res = text.match(/^\[CQ:at,qq=(\d+)\] ?来张色图$/)
         if (res && parseInt(res[1]) == message.self_id) {
-            send_pic(reply);
+            send_pic(reply)
         }
     } else if (message.message_type == "private") {
         if (text == "来张色图") {
-            send_pic(reply);
+            send_pic(reply)
         }
     }
-    return false;
+    return false
 }
 
 function send_pic(reply: (message: string) => void) {
     axios.get(API).then(({ data: data }) => {
-        reply(`${data.title}\n${data.userName} ${data.id}`);
+        reply(`${data.title}\n${data.userName} ${data.id}`)
         reply(image(data.image))
-    });
+    }).catch(e => {
+        reply(e.toString())
+    })
 }
