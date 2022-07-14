6f188b6d766ec5e2fed4cbf76811575a800f8c56
flaribbit <1149761294@qq.com>
2022-07-14 11:08:12 +0800

    更新依赖 优化cheerio写法 修改翻译命令格式避免和其他机器人冲突

--- plugins/translate.ts
+++ plugins/translate.ts
@@ -1,15 +1,15 @@
 import { Handle } from "../bot"
-import cheerio from "cheerio"
+import { load } from "cheerio"
 import axios from "axios"
 
 export const name = "翻译"
-export const help = ".en <word> 查询英语翻译\n.jp <word> 查询日语翻译"
+export const help = "/en <word> 查询英语翻译\n/jp <word> 查询日语翻译"
 export const handle: Handle = function (message, reply, info) {
     const text = message.message
     var res: RegExpMatchArray | null
-    if (res = text.match(/^\.en ?(.+)$/)) {
+    if (res = text.match(/^\/en ?(.+)$/)) {
         translate_en(res[1]).then(reply).catch(e => reply(e.message))
-    } else if (res = text.match(/^(?:\.jp|。ｊｐ) ?(.+)$/)) {
+    } else if (res = text.match(/^(?:\/jp|。ｊｐ) ?(.+)$/)) {
         translate_jp(res[1]).then(reply).catch(e => reply(e.message))
     }
 }
@@ -33,7 +33,7 @@ async function translate_jp(word: string): Promise<string> {
         headers: { cookie: "HJ_UID=40e16cb0-69f2-8b35-d09b-4a8627f5cf3b; HJ_SID=s063zt-a49b-40d0-b3dc-e3625b640f9f" }
     })
     const ret: string[] = []
-    const $ = cheerio.load(res.data)
+    const $ = load(res.data)
     $(".word-details-pane-header").each((_, e) => {
         const pane = $(e)
         ret.push(pane.find(".word-text>h2").text() + " " + pane.find(".pronounces>span:nth-child(1)").text())
